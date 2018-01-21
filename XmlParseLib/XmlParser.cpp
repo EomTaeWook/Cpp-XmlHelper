706614e65e6804b3dc0ee2ba54d0e018f24ef6e5
EomTaeWook <trim0917@gmail.com>
2018-01-21 22:30:55 +0900

    Update XmlParser.cpp

--- XmlParseLib/XmlParser.cpp
+++ XmlParseLib/XmlParser.cpp
@@ -300,7 +300,7 @@ char* CXmlParser::JsonToXml(Json::Value& node)
 	pStream->Read(pBuff, buffsize, &ulBytesRead);
 	pWriter->Release();
 	pStream->Release();
-	delete pWriter;
-	delete pStream;
+	pWriter = NULL;
+	pStream = NULL;
 	return pBuff;
 }
